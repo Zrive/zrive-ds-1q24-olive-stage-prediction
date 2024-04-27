9bc20349c54896dab3152fe8d4da5fcb710619f1
“Alvaro_Leal” <alvarolealdz@gmail.com>
2024-04-27 12:11:36 +0200

    train_test_split change

--- src/models.py
+++ src/models.py
@@ -2,28 +2,33 @@ import pandas as pd
 import numpy as np
 
 
-def train_test_split(df: pd.DataFrame, split_year: int = 2021) -> (pd.DataFrame, pd.DataFrame):
+def train_test_split(df: pd.DataFrame, split_year: int = 2021, max_year: int = None) -> (pd.DataFrame, pd.DataFrame):
     """
-    Divide un DataFrame en conjuntos de entrenamiento y prueba basado en un año específico.
+    Divide un DataFrame en conjuntos de entrenamiento y prueba basado en un año específico, con una opción para excluir
+    años posteriores a un máximo dado.
 
     Parámetros:
     df (pd.DataFrame): El DataFrame a dividir, que debe contener una columna de fecha de tipo datetime.
     split_year (int): El año que se usará como límite para dividir los datos. El conjunto de entrenamiento
-                      incluirá este año y años anteriores, mientras que el conjunto de prueba incluirá
-                      años posteriores.
+                      incluirá este año y años anteriores.
+    max_year (int, opcional): El último año para incluir en el conjunto de prueba. Todos los datos posteriores a este año
+                              serán excluidos del conjunto de prueba. Si no se especifica, se incluirán todos los datos posteriores a split_year.
 
     Devuelve:
     train (pd.DataFrame): DataFrame de entrenamiento que incluye el split_year y años anteriores.
-    test (pd.DataFrame): DataFrame de prueba que incluye años después del split_year.
+    test (pd.DataFrame): DataFrame de prueba que incluye años después del split_year hasta el max_year, si se especifica.
     """
 
     df['year'] = df['fecha'].dt.year
 
     train = df[df['year'] <= split_year].copy()
-    test = df[df['year'] > split_year].copy()
+    if max_year:
+        test = df[(df['year'] > split_year) & (df['year'] <= max_year)].copy()
+    else:
+        test = df[df['year'] > split_year].copy()
 
     return train, test
-# train, test = train_test_split(df, 2021)
+# train_set, test_set = train_test_split(df, split_year=2021, max_year=2022)
 
 
 def baseline(train: pd.DataFrame, test: pd.DataFrame, target_column: str):
